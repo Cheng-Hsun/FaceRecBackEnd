da79ecf2e7cd0c3d6ac235c440ec4f2073913244
s102034013 <xu3cj84wu0h9@gmail.com>
2019-02-18 23:57:24 -0800

    fix

--- server.js
+++ server.js
@@ -12,16 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
-		user: 'rex',
-		password: '',
-		database: 'smart-brain'
+		connectionString: process.env.DATABASE_URL,
+		ssl: true
 	}
-  // 
-  // connection: {
-  // 	connectionString: process.env.DATABASE_URL,
-  //   ssl: true
-  // }
 }); 
 
 // db.select('*').from('users').then(data => {
