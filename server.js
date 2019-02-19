f4320486e8ec9b43c93352339315841008bd2657
s102034013 <xu3cj84wu0h9@gmail.com>
2019-02-18 23:13:38 -0800

    try db

--- server.js
+++ server.js
@@ -10,11 +10,15 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-  client: 'pg',
-  connection: {
-  	connectionString: process.env.DATABASE_URL,
-    ssl: true
-  }
+	host: '127.0.0.1',
+	user: 'rex',
+	password: '',
+	database: 'smart-brain'
+  // client: 'pg',
+  // connection: {
+  // 	connectionString: process.env.DATABASE_URL,
+  //   ssl: true
+  // }
 }); 
 
 // db.select('*').from('users').then(data => {
